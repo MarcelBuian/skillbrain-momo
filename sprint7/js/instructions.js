06b999fe2f00ce72fbbd1fd0cc3eff1fe9d485b6
marcelbuian <marcel.buian@gdcgroup.com>
2024-10-02 22:05:15 +0200

    Am adaugat sprint 7

---
+++ sprint7/js/instructions.js
@@ -0,0 +1,61 @@
+// 1. if
+
+// IF (conditie) THEN
+//   do something
+// ELSE IF (alta conditie) THEN
+//   do some other thing
+// ELSE (nici una din cele 2 conditii) THEN
+//   do something else
+
+const a = 10;
+
+if (a > 0) {
+    console.log("a> 0 ?");
+    console.log("da, a este pozitiv");
+} else if (a === 0) {
+    console.log("a este zero");
+} else {
+    console.log("a nu este pozitiv");
+    console.log("a este negativ");
+}
+
+
+// 2. functii:
+// 2.a functie clasica:
+
+function get_sum(a, b) {
+    return a + b;
+}
+
+const n1 = 3;
+const n2 = 7;
+const n3 = get_sum(n1, n2);
+
+console.log(n3);
+console.log(n1 + " + " + n2 + " = " + n3);
+console.log(`${n1} + ${n2} = ${n3}`);
+
+function get_div(a, b) {
+    if (b === 0) {
+        throw "Nu se poate imparti la zero";
+    }
+
+    return a / b;
+}
+
+// console.log(get_div(4, 0));
+function get_pozitiv(n) {
+    if (n < 0) {
+        return -n;
+    }
+
+    return n;
+}
+
+console.log(get_pozitiv(-5));
+
+// functii de tip arrow TODO!
+get_sum = (a, b) => a + b;
+
+
+// 3. Switch (todo)
---
+++ sprint7/js/variables.js
@@ -0,0 +1,17 @@
+// console.log("text");
+// variabilele se declara de 2 tipuri:
+// 1. let (variabila se poate modifca)
+// 2. const (nu se modifica)
+
+// 3. var (deprecated - nu se mai folosete)
+
+
+
+const brandName = "skillbrain";
+let age = 2;
+
+console.log(brandName, age);
+
+age = 3;
+
+console.log(brandName, age);
